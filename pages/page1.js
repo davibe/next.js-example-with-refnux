d32f2e069a62b67b6558d0529616af5b78b75398
Davide Bertola <dade@dadeb.it>
2017-02-18 14:25:04 +0100

    initial import

---
+++ pages/page1.js
@@ -0,0 +1,18 @@
+import { Provider, connect } from 'refnux'
+import Link from 'next/link'
+import getStore from '../store/getStore'
+import counterIncrement from '../store/counterIncrement'
+
+const Index = connect(
+  (state, dispatch) =>
+    <div>
+      <h3>Page 1</h3>
+      <p>{JSON.stringify(state, null, 2)}</p>
+      <button onClick={() => dispatch(counterIncrement)} >Increment</button>
+      <Link href="/page2"><button>go to page 2</button></Link>
+    </div>
+)
+
+const App = () => <Provider store={getStore()} app={Index} />
+
+export default App
---
+++ pages/page2.js
@@ -0,0 +1,19 @@
+import { Provider, connect } from 'refnux'
+import Link from 'next/link'
+
+import getStore from '../store/getStore'
+import counterIncrement from '../store/counterIncrement'
+
+const Index = connect(
+  (state, dispatch) =>
+    <div>
+      <h3>Page 2</h3>
+      <p>Current state: {JSON.stringify(state, null, 2)}</p>
+      <button onClick={() => dispatch(counterIncrement)} >Increment</button>
+      <Link href="/page1"><button>go to page 2</button></Link>
+    </div>
+)
+
+const App = () => <Provider store={getStore()} app={Index} />
+
+export default App
