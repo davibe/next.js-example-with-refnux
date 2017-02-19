064091ed5848253acd94a34759dfbb8caa3b1418
Davide Bertola <dade@dadeb.it>
2017-02-19 09:53:07 +0100

    cleanup

--- pages/page1.js
+++ pages/page1.js
@@ -1,5 +1,6 @@
 import { Provider, connect } from 'refnux'
 import Link from 'next/link'
+
 import getStore from '../store/getStore'
 import counterIncrement from '../store/counterIncrement'
 
@@ -7,7 +8,7 @@ const Index = connect(
   (state, dispatch) =>
     <div>
       <h3>Page 1</h3>
-      <p>{JSON.stringify(state, null, 2)}</p>
+      <p>Current state: {JSON.stringify(state, null, 2)}</p>
       <button onClick={() => dispatch(counterIncrement)} >Increment</button>
       <Link href="/page2"><button>go to page 2</button></Link>
     </div>
